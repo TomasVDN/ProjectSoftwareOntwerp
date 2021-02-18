b6a5e82c3c3c5a389288184c6b985565cf11d0c7
Kobe Van der Linden <Kobe.vanderlinden@student.kuleuven.be>
2021-02-18 14:57:01 +0100

    Toevoegen lege functies

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 //dit is een test
+
+import java.io.*;
+
+import browsrhtml.*;
+//import java.net.URL.openStream;   Geeft error , weet niet waarom
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+    	String input = """
+				<a>
+				  <b x="foo"><cc xx="foo" yy="baz">Some text</c></b>
+				</a>
+				""";
+    	HtmlLexer lexer = new HtmlLexer(new StringReader(input));
+    }
+
 
 }
--- src/htmlElements/Hyperlink.java
+++ src/htmlElements/Hyperlink.java
@@ -1,5 +1,15 @@
 package htmlElements;
 
+import java.net.URL;
+
 public class Hyperlink {
+	
+	/**
+	 * Look at the href attribute of the hyperLink to obtain the full URL
+	 */
+	public URL activateHyperLink() {
+		return null;
+	}
+	
 
 }
