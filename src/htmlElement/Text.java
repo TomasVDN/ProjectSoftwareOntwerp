6c2b1a00ef7f3ff0b2f3f121f1a9317b23811636
Gebruiker <Gebruiker@192.168.1.53>
2021-02-21 18:01:47 +0100

    overloaden van de Text() constructor en de constructText() methode om wat te testen

--- src/htmlElement/Text.java
+++ src/htmlElement/Text.java
@@ -7,12 +7,19 @@ import java.awt.Graphics;
 public class Text extends GUIElement{
 	
 	private String text;
-	private Font font = new Font(Font.DIALOG, Font.PLAIN, 40);
+	private Font font = new Font(Font.DIALOG, Font.PLAIN, 20);
 
 	public Text(int x, int y, int w, int h, String text){
 		super(x, y, w, h);
 		this.setText(text);
 	}
+	
+	public Text(int x, int y, int w, int h, String text, Font font){
+		super(x, y, w, h);
+		this.setText(text);
+		this.font = font; 
+	}
+	
 	/**
 	 * automatically calculate the needed heigth and width to construct a Text object
 	 * @param string
@@ -21,10 +28,24 @@ public class Text extends GUIElement{
 	 * @param metrics
 	 * @return
 	 */
-	public static Text constructText(String string,int x, int y,FontMetrics metrics) {
+	public static Text constructText(String string,int x, int y, FontMetrics metrics) {
 	    //int textWidth = metrics.stringWidth(string);
-	    //int textHeigth =metrics.getHeight();
-	    return  new Text(x, y, 40, 40, string);
+	    //int textHeigth = metrics.getHeight();
+	    return new Text(x, y, 0, 5, string, new Font(Font.DIALOG, Font.PLAIN, 20));
+	}
+	
+	/**
+	 * automatically calculate the needed heigth and width to construct a Text object
+	 * @param string
+	 * @param x
+	 * @param y
+	 * @param metrics
+	 * @return
+	 */
+	public static Text constructText(String string,int x, int y, int h) {
+	    //int textWidth = metrics.stringWidth(string);
+	    //int textHeigth = metrics.getHeight();
+	    return new Text(x, y, 0, h, string, new Font(Font.DIALOG, Font.PLAIN, h));
 	}
 	
 	
