c0b1b4b33ad7e44febad15985c724eadc5e23e5c
TomasVDN <tomas.vandennoortgate@student.kuleuven.be>
2021-02-23 18:13:12 +0100

    made cursor, with delete, end, home, back space and charachter addition support. Fully documented.

--- src/htmlElement/TextBox.java
+++ src/htmlElement/TextBox.java
@@ -9,7 +9,7 @@ import canvaswindow.MyCanvasWindow;
 
 public class TextBox extends GUIElement{
 
-	private Text text;
+	private TextCursor text;
 	private Box box;
 	private boolean isActive;
 	private SurroundingTextBox selectedTextBox;
@@ -20,9 +20,8 @@ public class TextBox extends GUIElement{
 		super(x, y, w,  h + (h/4));
 		Color color = Color.white;
 		this.setBox(new Box(x, y, w, h + (h/4), color));
-		Text text = Text.constructText("input text: ", x, y, h);
-		this.setText(text);
-		this.setSelectedTextBox(new SurroundingTextBox(0, 0, 0, 0, Color.blue, text)); // maakt een selected textbox aan deze gaat initieel leeg zijn
+		this.text = new TextCursor(x, y, w, h, "Test", " Test2");
+		this.setSelectedTextBox(new SurroundingTextBox(0, 0, 0, 0, Color.blue, this.text.getText())); // maakt een selected textbox aan deze gaat initieel leeg zijn
 		this.window = window;
 	}
 	
@@ -37,29 +36,7 @@ public class TextBox extends GUIElement{
 	
 	public Box getBox() {
 		return this.box;
-	}
-	
-	/**
-	 * Sets the value text of this class
-	 * 
-	 * @param newText - new value of this.text
-	 */
-	public void setText(Text newText) {
-		this.text = newText;
-	}
-	
-	public Text getText() {
-		return this.text;
-	}
-
-	public void setTextValue(String newText) {
-		this.text.setText(newText);
-	}
-	
-	public String getTextValue() {
-		return this.text.getText();
-	}
-	
+	}	
 	
 	public boolean isActive() {
 		return isActive;
@@ -67,18 +44,33 @@ public class TextBox extends GUIElement{
 
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
+		this.getTextCursor().setTextCursorOn(isActive);
+	}
+	
+	/**
+	 * @return the text
+	 */
+	public TextCursor getTextCursor() {
+		return text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setTextCursor(TextCursor text) {
+		this.text = text;
 	}
 	
 	@Override
 	public void paint(Graphics g) {
 		this.getBox().paint(g);
 		this.getSelectedTextBox().paint(g);
-		this.getText().paint(g);
+		this.getTextCursor().paint(g);
 	}
 	
 	@Override
 	public void update(Graphics g) {
-		this.getText().update(g);
+		this.getTextCursor().update(g);
 	}
 	
 	
@@ -89,7 +81,7 @@ public class TextBox extends GUIElement{
 					this.getSelectedTextBox().unselectAllText();
 				}
 				else {
-					this.getSelectedTextBox().selectGivenText(this.getText());
+					this.getSelectedTextBox().selectGivenText(this.getTextCursor().getText());
 				}
 				this.setActive(true);
 				this.getBox().setColor(Color.gray);
@@ -107,7 +99,7 @@ public class TextBox extends GUIElement{
 	
 	/**
 	 * If the bar is focused, reroute to the functions of corresponding key code.
-	 * @param id - 
+	 * @param id
 	 * @param keyCode
 	 * @param keyChar
 	 */
@@ -115,21 +107,36 @@ public class TextBox extends GUIElement{
 		if (this.isActive()) {
 			if (id == KeyEvent.KEY_PRESSED) {
 				
-				switch (keyChar) {
-				case KeyEvent.VK_BACK_SPACE:
-					this.getSelectedTextBox().clearSelected();
+				switch (keyCode) {
+				case 8: //backspace
+					//this.getSelectedTextBox().clearSelected();
 					this.handleBackSpace();
 					break;
-				case KeyEvent.VK_ENTER:
+				case 127: //delete
+					this.handleDelete();
+					break;
+				case 21: //etner
 					this.handleEnter();
 					break;
-				case KeyEvent.VK_ESCAPE:
+				case 27: //escape
 					this.handleEscape();
+					break;
+				case 37: //left
+					this.handleLeft();
+					break;
+				case 39: //Right
+					this.handleRight();
+					break;
+				case 36: //home
+					this.handleHome();
+					break;
+				case 35: //end
+					this.handleEnd();
 					break;
 				default:
 					 if (keyChar != KeyEvent.CHAR_UNDEFINED) {
-						 	this.getSelectedTextBox().clearSelected();
-							this.setTextValue(this.getTextValue() + keyChar);
+						 	//this.getSelectedTextBox().clearSelected();
+							this.handleUndefined(keyChar);
 						}
 					break;
 				}
@@ -141,20 +148,56 @@ public class TextBox extends GUIElement{
 	 * Does the needed actions for the backSpace key.
 	 */
 	private void handleBackSpace() {
-		int textLength = this.getTextValue().length();
-		if (textLength > 0) { // if text is not empty
-			this.setTextValue(getTextValue().substring(0, textLength - 1));
-		}
+		this.getTextCursor().deletePrevious();
+	}
+	
+	/**
+	 * Does the needed actions for the backSpace key.
+	 */
+	private void handleDelete() {
+		this.getTextCursor().deleteNext();
+	}
+	
+	/**
+	 * Does the needed actions for the Left arrow key.
+	 */
+	private void handleLeft() {
+		this.getTextCursor().moveLeft();
+	}
+	
+	/**
+	 * Does the needed actions for the Right arrow key.
+	 */
+	private void handleRight() {
+		this.getTextCursor().moveRight();
+	}
+	
+	/**
+	 * Does the needed actions for the End key.
+	 */
+	private void handleEnd() {
+		this.getTextCursor().moveToEnd();
+	}
+	
+	/**
+	 * Does the needed actions for the home key.
+	 */
+	private void handleHome() {
+		this.getTextCursor().moveToBegin();
+	}
+	
+	/**
+	 * Does the needed actions for the undifined keys.
+	 */
+	private void handleUndefined(char keyChar) {
+		this.getTextCursor().addCharachter(keyChar);;
 	}
 	
 	/**
 	 * Does the needed actions for the enter key.
 	 */
 	private void handleEnter() {
-		int textLength = this.getTextValue().length();
-		if (textLength > 0) { // if text is not empty
-			this.getWindow().readFile(getTextValue());
-		}
+		this.getWindow().readFile(this.getTextCursor().getText().getText()); //TODO getText.getText is niet het mooiste ooit
 	}
 	
 	/**
@@ -190,3 +233,197 @@ public class TextBox extends GUIElement{
 
 
 }
+//
+//package htmlElement;
+//
+//import java.awt.Color;
+//import java.awt.Graphics;
+//import java.awt.event.KeyEvent;
+//import java.awt.event.MouseEvent;
+//
+//import canvaswindow.MyCanvasWindow;
+//
+//public class TextBox extends GUIElement{
+//
+//	private Text text;
+//	private Box box;
+//	private boolean isActive;
+//	private SurroundingTextBox selectedTextBox;
+//	private MyCanvasWindow window;
+//	
+//	
+//	public TextBox(int x, int y, int w, int h, MyCanvasWindow window) {
+//		super(x, y, w,  h + (h/4));
+//		Color color = Color.white;
+//		this.setBox(new Box(x, y, w, h + (h/4), color));
+//		Text text = Text.constructText("input text: ", x, y, h);
+//		this.setText(text);
+//		this.setSelectedTextBox(new SurroundingTextBox(0, 0, 0, 0, Color.blue, text)); // maakt een selected textbox aan deze gaat initieel leeg zijn
+//		this.window = window;
+//	}
+//	
+//	/**
+//	 * Sets the value box of this class
+//	 * 
+//	 * @param newBox - new value of this.box
+//	 */
+//	public void setBox(Box newBox) {
+//		this.box = newBox;
+//	}
+//	
+//	public Box getBox() {
+//		return this.box;
+//	}
+//	
+//	/**
+//	 * Sets the value text of this class
+//	 * 
+//	 * @param newText - new value of this.text
+//	 */
+//	public void setText(Text newText) {
+//		this.text = newText;
+//	}
+//	
+//	public Text getText() {
+//		return this.text;
+//	}
+//
+//	public void setTextValue(String newText) {
+//		this.text.setText(newText);
+//	}
+//	
+//	public String getTextValue() {
+//		return this.text.getText();
+//	}
+//	
+//	
+//	public boolean isActive() {
+//		return isActive;
+//	}
+//
+//	public void setActive(boolean isActive) {
+//		this.isActive = isActive;
+//	}
+//	
+//	@Override
+//	public void paint(Graphics g) {
+//		this.getBox().paint(g);
+//		this.getSelectedTextBox().paint(g);
+//		this.getText().paint(g);
+//	}
+//	
+//	@Override
+//	public void update(Graphics g) {
+//		this.getText().update(g);
+//	}
+//	
+//	
+//	public void handleMouseEvent(int id, int x, int y) {
+//		if (id == MouseEvent.MOUSE_CLICKED) {
+//			if (this.checkCoordinates(x, y)) {
+//				if(this.isActive()) {
+//					this.getSelectedTextBox().unselectAllText();
+//				}
+//				else {
+//					this.getSelectedTextBox().selectGivenText(this.getText());
+//				}
+//				this.setActive(true);
+//				this.getBox().setColor(Color.gray);
+//			} else {
+//				if (this.isActive()){
+//					this.handleEnter();
+//					this.getSelectedTextBox().unselectAllText();
+//				}
+//				this.setActive(false);
+//				this.getBox().setColor(Color.white);
+//			}
+//			
+//		}
+//	}
+//	
+//	/**
+//	 * If the bar is focused, reroute to the functions of corresponding key code.
+//	 * @param id - 
+//	 * @param keyCode
+//	 * @param keyChar
+//	 */
+//	public void handleKeyBoardEvent(int id,int keyCode, char keyChar) {
+//		if (this.isActive()) {
+//			if (id == KeyEvent.KEY_PRESSED) {
+//				
+//				switch (keyChar) {
+//				case KeyEvent.VK_BACK_SPACE:
+//					this.getSelectedTextBox().clearSelected();
+//					this.handleBackSpace();
+//					break;
+//				case KeyEvent.VK_ENTER:
+//					this.handleEnter();
+//					break;
+//				case KeyEvent.VK_ESCAPE:
+//					this.handleEscape();
+//					break;
+//				default:
+//					 if (keyChar != KeyEvent.CHAR_UNDEFINED) {
+//						 	this.getSelectedTextBox().clearSelected();
+//							this.setTextValue(this.getTextValue() + keyChar);
+//						}
+//					break;
+//				}
+//			}
+//		}
+//	}
+//	
+//	/**
+//	 * Does the needed actions for the backSpace key.
+//	 */
+//	private void handleBackSpace() {
+//		int textLength = this.getTextValue().length();
+//		if (textLength > 0) { // if text is not empty
+//			this.setTextValue(getTextValue().substring(0, textLength - 1));
+//		}
+//	}
+//	
+//	/**
+//	 * Does the needed actions for the enter key.
+//	 */
+//	private void handleEnter() {
+//		int textLength = this.getTextValue().length();
+//		if (textLength > 0) { // if text is not empty
+//			this.getWindow().readFile(getTextValue());
+//		}
+//	}
+//	
+//	/**
+//	 * Does the needed actions for the escape key.
+//	 */
+//	private void handleEscape() {
+//		this.setActive(false);
+//		this.getBox().setColor(Color.white);
+//	}
+//
+//	public SurroundingTextBox getSelectedTextBox() {
+//		return selectedTextBox;
+//	}
+//
+//	public void setSelectedTextBox(SurroundingTextBox selectedText) {
+//		this.selectedTextBox = selectedText;
+//	}
+//	
+//	/**
+//	 * @return the window
+//	 */
+//	public MyCanvasWindow getWindow() {
+//		return window;
+//	}
+//
+//	/**
+//	 * @param window - the window to set
+//	 */
+//	public void setWindow(MyCanvasWindow window) {
+//		this.window = window;
+//	}
+//
+//
+//
+//}
+
---
+++ src/htmlElement/TextCursor.java
@@ -0,0 +1,228 @@
+package htmlElement;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class TextCursor extends GUIElement {
+	
+	private Text leftText;
+	private Text rightText;
+	private Box box;
+	private boolean textCursorOn;
+
+
+	/**
+	 * Constructor of the TextCursor class. TextCursor is constructed in the following way: Text + Box + Text.
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @param h
+	 * @param color
+	 * @param leftString
+	 * @param rightString
+	 */
+	public TextCursor(int x, int y, int w, int h, String leftString,String rightString) {
+		super(x, y, w, h);
+		
+		leftText = new Text(x, y, w, h, leftString);
+		rightText = new Text(x, y, w, h, rightString);
+		
+		box = new Box(x, y, (int) Math.ceil(this.getLeftText().getFont().getSize() / 10), this.getLeftText().getFont().getSize(), Color.black);
+	}
+
+	/**
+	 * This method paints the two Text's. If, additionally, this.isTextCursor is true, it will also paint the cursor in itself.
+	 */
+	@Override
+	public void paint(Graphics g) {
+		this.getLeftText().paint(g);
+		this.getRightText().paint(g);
+		
+		if(this.isTextCursorOn()) {
+			this.getBox().paint(g);
+		}
+	}
+	
+	/**
+	 * This method updates the position of the right Text and of the box depending on the size of the left Text.
+	 */
+	public void update(Graphics g) {
+		this.getLeftText().update(g);
+		
+		int rightXLeftSide = this.getLeftText().getRightX();
+		int upperYLeftSide = this.getLeftText().getUpperY();
+		
+		this.getBox().setX(rightXLeftSide);
+		this.getBox().setY(this.getLeftText().getLowerY() - this.getBox().getHeight());
+		
+		this.getRightText().setX(rightXLeftSide);
+		this.getRightText().setY(upperYLeftSide);
+		
+		this.getRightText().update(g);
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when moving one character to the right.
+	 */
+	public void moveRight() {
+		if (this.getStringRightText().length() != 0) {
+			this.setLeftText(this.getStringLeftText() + this.getStringRightText().charAt(0));
+			this.setrightText(this.getStringRightText().substring(1));
+		}		
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when moving one character to the left.
+	 */
+	public void moveLeft() {
+		if (this.getStringLeftText().length() != 0) {
+			this.setrightText(this.getStringLeftText().charAt(this.getStringLeftText().length() - 1) + this.getStringRightText());
+			this.setLeftText(this.getStringLeftText().substring(0, this.getStringLeftText().length() - 1));
+		}
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when moving to the end of the text.
+	 */
+	public void moveToEnd() {
+		this.setLeftText(this.getStringLeftText() + this.getStringRightText());
+		this.setrightText("");
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when moving to the begin of the text.
+	 */
+	public void moveToBegin() {
+		this.setrightText(this.getStringLeftText() + this.getStringRightText());
+		this.setLeftText("");
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when removing a character on the left of the cursor.
+	 */
+	public void deletePrevious() {
+		this.setLeftText(this.getStringLeftText().substring(0, this.getStringLeftText().length() - 1));
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when removing a character on the right of the cursor.
+	 */
+	public void deleteNext() {
+		this.setrightText(this.getStringRightText().substring(1));
+	}
+	
+	/**
+	 * This method handles every action that needs to be done when adding a character. This always happens on the left of the cursor.
+	 */
+	public void addCharachter(char keyChar) {
+		this.setLeftText(this.getLeftText().getText() + keyChar);
+	}
+	
+	/**
+	 * This method returns the text stored in the left Text element.
+	 * @return this.leftText.getText
+	 */
+	public String getStringLeftText() {
+		return this.leftText.getText();
+	}
+
+	/**
+	 * This method sets the text value of the left Text element.
+	 * @param leftText - the new value of text of the left Text element.
+	 */
+	public void setLeftText(String leftText) {
+		this.leftText.setText(leftText);;
+	}
+
+	/**
+	 * This method returns the text stored in the right Text element.
+	 * @return this.leftText.getText
+	 */
+	public String getStringRightText() {
+		return rightText.getText();
+	}
+
+	/**
+	 * This method sets the text value of the right Text element.
+	 * @param rightText - the new value of text of the right Text element.
+	 */
+	public void setrightText(String rightText) {
+		this.rightText.setText(rightText);
+	}
+
+	/**
+	 * This method returns a new Text element containing all the text contained in both Text elements
+	 * @return new Text(this.leftText.getLeftX(), this.leftText.getUpperY(), this.leftText.getWidth() + this.rightText.getWidth(), this.leftText.getHeight(), this.getStringLeftText() + this.getStringRightText())
+	 */
+	public Text getText() {
+		return new Text(this.leftText.getLeftX(), this.leftText.getUpperY(), this.leftText.getWidth() + this.rightText.getWidth(),
+				this.leftText.getHeight(), this.getStringLeftText() + this.getStringRightText());
+	}
+	
+	/**
+	 * This method returns if the cursor is to be displayed
+	 * @return this.textCursorOn
+	 */
+	public boolean isTextCursorOn() {
+		return this.textCursorOn;
+	}
+
+	/**
+	 * This method sets the value of textCursorOn. If True, the cursor will be displayed. Otherwise, it will not.
+	 * @param textCursorOn
+	 */
+	public void setTextCursorOn(boolean textCursorOn) {
+		this.textCursorOn = textCursorOn;
+	}
+	
+	/**
+	 * This methods returns the left Text element.
+	 * @return this.leftText 
+	 */
+	public Text getLeftText() {
+		return this.leftText;
+	}
+	
+	/**
+	 * This method sets the left Text element to the given Text element.
+	 * @param leftText
+	 */
+	public void setLeftText(Text leftText) {
+		this.leftText = leftText;
+	}
+	
+	/**
+	 * This methods returns the right Text element.
+	 * @return this.rightText 
+	 */
+	public Text getRightText() {
+		return this.rightText;
+	}
+	
+	/**
+	 * This method sets the right Text element to the given Text element.
+	 * @param rightText
+	 */
+	public void setRightText(Text rightText) {
+		this.rightText = rightText;
+	}
+	
+	/**
+	 * This methods sets the box element to the given box element.
+	 * @param box 
+	 */
+	public void setBox(Box box) {
+		this.box = box;
+	}
+	
+	/**
+	 * This methods returns the box element.
+	 * @return this.box 
+	 */
+	public Box getBox() {
+		return this.box;
+	}
+	
+
+
+}
