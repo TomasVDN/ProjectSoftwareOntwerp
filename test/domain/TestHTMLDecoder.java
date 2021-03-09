1c74cfc86c522c244f6b524727f78e16d24e6c78
StevenHuygens <stevenhuygens13@gmail.com>
2021-03-09 16:35:10 +0100

    Updated test structure

---
+++ test/domain/TestHTMLDecoder.java
@@ -0,0 +1,14 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestHTMLDecoder {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/events/TestRunUrlEvent.java
@@ -0,0 +1,14 @@
+package events;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRunUrlEvent {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
