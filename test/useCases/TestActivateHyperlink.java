d43ad50b1edd1f7a6d78e238d432ce895b3eb91b
StevenHuygens <stevenhuygens13@gmail.com>
2021-03-09 16:40:40 +0100

    Added structure for the required tests on the use cases

---
+++ test/useCases/TestActivateHyperlink.java
@@ -0,0 +1,14 @@
+package useCases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestActivateHyperlink {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/useCases/TestEnterURL.java
@@ -0,0 +1,14 @@
+package useCases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestEnterURL {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
