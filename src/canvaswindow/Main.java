14cadf17f74b89501ce8bae792d2d31843555d0f
TomasVDN <tomas.vandennoortgate@student.kuleuven.be>
2021-03-11 20:56:28 +0100

    comments and some other stuff

--- src/canvaswindow/Main.java
+++ src/canvaswindow/Main.java
@@ -5,9 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		      java.awt.EventQueue.invokeLater(() -> {
 		         MyCanvasWindow window = new MyCanvasWindow("Browsr");
-		         //window.show();
-		         //window.recordSession("recordings/recording");
-		         //MyCanvasWindow.replayRecording("recordings/recording", window);
 		         window.show();
 		      });
 	}
