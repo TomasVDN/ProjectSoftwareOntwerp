ad53d222730fda33f7f80cb00c4b54b1659d3a8d
Kobe Van der Linden <Kobe.vanderlinden@student.kuleuven.be>
2021-03-11 19:11:51 +0100

    tests for tableRow

---
+++ test/GUIElements/TestTableRow.java
@@ -0,0 +1,59 @@
+package GUIElements;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTableRow {
+
+	Text textGUI = new Text(0, 0, "");
+	Text mediumText = new Text(0,0,"bla");
+	Text mediumText2 = new Text(0,0,"blabla");
+	Text textGUIaLotOfText = new Text(0, 0, "blablablablabla");
+	Text hyperText = new Text(0,0,"This is a hyperlink");
+	Hyperlink hyper = new Hyperlink(0, 0, hyperText, "https://blabla.com");
+	TableCellGUI cell1 = new TableCellGUI(hyper, 0, 0, 0, 0);
+	TableCellGUI cell2 = new TableCellGUI(textGUIaLotOfText, 0, 0, 0, 0);
+	
+	@Test
+	void testGetWidth() {
+		ArrayList<TableCellGUI> tableRow = new ArrayList<>();
+		tableRow.add(cell1);
+		tableRow.add(cell2);
+		TableRowGUI row = new TableRowGUI(tableRow, 0, 0, 0, 0);
+		ArrayList<TableRowGUI> tableRowList=new ArrayList<>();;
+		tableRowList.add(row);
+		TableGUI guiTable = new TableGUI(tableRowList, 0, 0, 0, 0); // checks if width of array is correct after update
+		assertEquals(row.getWidth(), hyper.getWidth()+textGUIaLotOfText.getWidth());
+		
+	}
+
+	@Test
+	void testGetHeight() {
+		ArrayList<TableCellGUI> tableRow = new ArrayList<>();
+		tableRow.add(cell1);
+		tableRow.add(cell2);
+		TableRowGUI row = new TableRowGUI(tableRow, 0, 0, 0, 0);
+		ArrayList<TableRowGUI> tableRowList=new ArrayList<>();;
+		tableRowList.add(row);
+		TableGUI guiTable = new TableGUI(tableRowList, 0, 0, 0, 0); // checks if height of array is correct after update
+		assertEquals(row.getHeight(), hyper.getHeight());
+	}
+
+	@Test
+	void testGetGUIAtPosition() {
+		ArrayList<TableCellGUI> tableRow = new ArrayList<>();
+		tableRow.add(cell1);
+		tableRow.add(cell2);
+		TableRowGUI row = new TableRowGUI(tableRow, 0, 0, 0, 0);
+		ArrayList<TableRowGUI> tableRowList=new ArrayList<>();;
+		tableRowList.add(row);
+		TableGUI guiTable = new TableGUI(tableRowList, 0, 0, 0, 0);
+		assertEquals(guiTable.getGUIAtPosition(hyper.getWidth()+1, 0),textGUIaLotOfText);
+	}
+
+	
+}
