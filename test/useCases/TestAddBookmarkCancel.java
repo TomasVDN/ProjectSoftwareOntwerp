4879806beaf78841df2a50ba7131611b6d387498
StevenHuygens <stevenhuygens13@gmail.com>
2021-04-16 10:28:22 +0200

    initial commit

---
+++ test/useCases/TestAddBookmarkCancel.java
@@ -0,0 +1,14 @@
+package useCases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAddBookmarkCancel {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
