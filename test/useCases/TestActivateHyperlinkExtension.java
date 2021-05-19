14b3c8fa808d0ccaf6427d1ad8f876a530fa98ec
StevenHuygens <stevenhuygens13@gmail.com>
2021-05-19 11:37:01 +0200

    fix

--- test/useCases/TestActivateHyperlinkExtension.java
+++ test/useCases/TestActivateHyperlinkExtension.java
@@ -12,16 +12,17 @@ import org.junit.jupiter.api.Test;
 import GUIElements.Text;
 import canvaswindow.MyCanvasWindow;
 import GUIElements.Container;
+import GUIElements.HTMLDocument;
 
 
 class TestActivateHyperlinkExtension {
 
-	private MyCanvasWindow window;
+	private MyCanvasWindow mainWindow;
 	
 	@BeforeEach
 	void setUp() throws InvocationTargetException, InterruptedException {
 		java.awt.EventQueue.invokeAndWait(() -> {
-			window = new MyCanvasWindow("BrowsrController");
+			mainWindow = new MyCanvasWindow("BrowsrController");
 		});
 	}
 	
@@ -29,34 +30,34 @@ class TestActivateHyperlinkExtension {
 	@DisplayName("Use Case 4.1 Extension 2a: Activate Hyperlink")
 	void test() {
 		//load the page for the test
-		window.getWindowManager().getSearchbar().replaceBox("https://konikoko.github.io/");
-		window.getWindowManager().getSearchbar().handleEnter();
+		mainWindow.getWindowManager().getSearchbar().replaceBox("https://konikoko.github.io/");
+		mainWindow.getWindowManager().getSearchbar().handleEnter();
 		
 		//click on the broken the broken hyperlink
-		window.handleMouseEvent(MouseEvent.MOUSE_PRESSED, 70, 180, 1, MouseEvent.BUTTON1, 0);
-		window.handleMouseEvent(MouseEvent.MOUSE_RELEASED, 70, 180, 1, MouseEvent.BUTTON1, 0);
-		window.handleMouseEvent(MouseEvent.MOUSE_CLICKED, 70, 180, 1, MouseEvent.BUTTON1, 0);
+		mainWindow.handleMouseEvent(MouseEvent.MOUSE_PRESSED, 70, 180, 1, MouseEvent.BUTTON1, 0);
+		mainWindow.handleMouseEvent(MouseEvent.MOUSE_RELEASED, 70, 180, 1, MouseEvent.BUTTON1, 0);
+		mainWindow.handleMouseEvent(MouseEvent.MOUSE_CLICKED, 70, 180, 1, MouseEvent.BUTTON1, 0);
 
 		//check the error screen
-		Container pageContainer = window.getWindowManager().getMainDialog().getDocumentArea();
-		Text pageErrorText = (Text) (pageContainer.getElements().get(0));
+		HTMLDocument htmlDocument = mainWindow.getWindowManager().getMainDialog().getActiveHTMLDocument();
+		Text pageErrorText = (Text) (htmlDocument.getElements().get(0));
 		
 		assertEquals("Error occured. Make sure you entered a valid URL.", pageErrorText.getText());
 		
 		//load the page for the test
-		window.getWindowManager().getSearchbar().replaceBox("https://konikoko.github.io/");
-		window.getWindowManager().getSearchbar().handleEnter();
+		mainWindow.getWindowManager().getSearchbar().replaceBox("https://konikoko.github.io/");
+		mainWindow.getWindowManager().getSearchbar().handleEnter();
 		
 		//click on the working hyperlink but with the broken html file
-		window.handleMouseEvent(MouseEvent.MOUSE_PRESSED, 37, 205, 1, MouseEvent.BUTTON1, 0);
-		window.handleMouseEvent(MouseEvent.MOUSE_RELEASED, 37, 205, 1, MouseEvent.BUTTON1, 0);
-		window.handleMouseEvent(MouseEvent.MOUSE_CLICKED, 37, 205, 1, MouseEvent.BUTTON1, 0);
+		mainWindow.handleMouseEvent(MouseEvent.MOUSE_PRESSED, 37, 217, 1, MouseEvent.BUTTON1, 0);
+		mainWindow.handleMouseEvent(MouseEvent.MOUSE_RELEASED, 37, 217, 1, MouseEvent.BUTTON1, 0);
+		mainWindow.handleMouseEvent(MouseEvent.MOUSE_CLICKED, 37, 217, 1, MouseEvent.BUTTON1, 0);
 
 		//check the error screen
-		Container pageContainer1 = window.getWindowManager().getMainDialog().getDocumentArea();
-		Text pageErrorText1 = (Text) (pageContainer1.getElements().get(0));
+		HTMLDocument htmlDocument1 = mainWindow.getWindowManager().getMainDialog().getActiveHTMLDocument();
+		Text pageErrorText1 = (Text) (htmlDocument.getElements().get(0));
 		
-		assertEquals("Error occured. Reason: not a valid BrowsrController document.", pageErrorText1.getText());
+		assertEquals("Error occured. Reason: not a valid Browsr document.", pageErrorText1.getText());
 	}
 
 }
