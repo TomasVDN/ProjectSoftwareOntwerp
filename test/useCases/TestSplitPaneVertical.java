4073bc452a50d02568eec8a127b692a4de783a18
StevenHuygens <stevenhuygens13@gmail.com>
2021-05-19 21:30:14 +0200

    vreemd

--- test/useCases/TestSplitPaneVertical.java
+++ test/useCases/TestSplitPaneVertical.java
@@ -22,7 +22,9 @@ import GUIElements.Button;
 import GUIElements.GUIElement;
 import GUIElements.HTMLDocument;
 import GUIElements.SaveDialog;
+import GUIElements.ScrollableHTMLDocument;
 import GUIElements.ScrollableTextBox;
+import GUIElements.SplitHTMLDocument;
 import GUIElements.Text;
 import GUIElements.TextBox;
 import canvaswindow.MyCanvasWindow;
@@ -48,36 +50,45 @@ private MyCanvasWindow mainWindow;
 		assertEquals(mainWindow.getWindowManager().getMainDialog(), mainWindow.getWindowManager().getActiveDialog());
 		assertEquals(null, mainWindow.getWindowManager().getElementWithKeyboardFocus());
 		
-		//calculate the click coordinates
+		// calculate the click coordinates
 		HTMLDocument originalDocument = mainWindow.getWindowManager().getMainDialog().getDocumentArea().getActiveHTMLDocument();
 		int yHeight = originalDocument.getY() + (originalDocument.getHeight() / 2);
 		int xWidthLeftPane = originalDocument.getX() + (originalDocument.getWidth() / 2);
 		int xWidthRightPane = ((3 * originalDocument.getWidth()) / 4) + originalDocument.getX();
 				
-		//click on control H
+		// click on control H
 		mainWindow.handleKeyEvent(KeyEvent.KEY_PRESSED, 17, '?', 128); 
 		mainWindow.handleKeyEvent(KeyEvent.KEY_PRESSED, 72, 'h', 128);
 		mainWindow.handleKeyEvent(KeyEvent.KEY_RELEASED, 72, 'h', 128);
 		mainWindow.handleKeyEvent(KeyEvent.KEY_RELEASED, 17, '?', 128);
+		
+		SplitHTMLDocument root = (SplitHTMLDocument) mainWindow.getWindowManager().getMainDialog().getDocumentArea();
+		HTMLDocument leftPanel = ((ScrollableHTMLDocument) root.getLeftPanel()).getHtmlDocument();
+		HTMLDocument rightPanel = ((ScrollableHTMLDocument) root.getRightPanel()).getHtmlDocument();
 		
 		//click on the left pane
 		mainWindow.handleMouseEvent(MouseEvent.MOUSE_PRESSED, xWidthLeftPane, yHeight, 1, MouseEvent.BUTTON1, 0);
 		mainWindow.handleMouseEvent(MouseEvent.MOUSE_RELEASED, xWidthLeftPane, yHeight, 1, MouseEvent.BUTTON1, 0);
 		mainWindow.handleMouseEvent(MouseEvent.MOUSE_CLICKED, xWidthLeftPane, yHeight, 1, MouseEvent.BUTTON1, 0);
 		
-		//save the left pane
-		HTMLDocument leftPane = mainWindow.getWindowManager().getMainDialog().getDocumentArea().getActiveHTMLDocument();
+		assertEquals(leftPanel, mainWindow.getWindowManager().getMainDialog().getActiveHTMLDocument());
+		assertEquals(true, leftPanel.isActive());
+		assertEquals(false, rightPanel.isActive());
 		
 		//click on the right pane
 		mainWindow.handleMouseEvent(MouseEvent.MOUSE_PRESSED, xWidthRightPane, yHeight, 1, MouseEvent.BUTTON1, 0);
 		mainWindow.handleMouseEvent(MouseEvent.MOUSE_RELEASED, xWidthRightPane, yHeight, 1, MouseEvent.BUTTON1, 0);
 		mainWindow.handleMouseEvent(MouseEvent.MOUSE_CLICKED, xWidthRightPane, yHeight, 1, MouseEvent.BUTTON1, 0);
 		
-		//save the right pane
-		HTMLDocument rightPane = mainWindow.getWindowManager().getMainDialog().getDocumentArea().getActiveHTMLDocument();
+		assertEquals(rightPanel, mainWindow.getWindowManager().getMainDialog().getActiveHTMLDocument());
+		assertEquals(true, rightPanel.isActive());
+		assertEquals(false, leftPanel.isActive());
+
 		
 		//check if the panes are different
-		assertNotEquals(leftPane, rightPane,"The original pane has not been split correctly");
+		assertNotEquals(leftPanel, rightPanel,"The original pane has not been split correctly");
+		// check if they contain the same HTMLcode
+		assertEquals(leftPanel.getHTMLCode(), rightPanel.getHTMLCode());
 		
 	}
 	
