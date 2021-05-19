939b1764256701a337ed49b3130b21e7c9555bd3
StevenHuygens <stevenhuygens13@gmail.com>
2021-05-19 09:48:53 +0200

    fix for textBox attribute never getting initialized

--- src/GUIElements/ScrollableTextBox.java
+++ src/GUIElements/ScrollableTextBox.java
@@ -3,29 +3,31 @@ package GUIElements;
 public class ScrollableTextBox extends Container {
 	
 	public static final int DEFAULT_SCROLLBAR_SMALL_SIZE = 25;
-	private TextBox textBox;
+	TextBox textBox;
 	
 	public ScrollableTextBox(int x, int y, int w, int h) {
 		super(x,y,w,h+10);
-		TextBox textbox= new TextBox(x, y, w, h);
-		ScrollBar scrollBar = new ScrollBar(Direction.HORIZONTAL, x, textbox.getHeight(), w, 10, textbox.getWidth(), textbox.getWidthText());
-		textbox.addAdjustListener(scrollBar);
-		scrollBar.addScrollBarListener(textbox);
-		this.addElement(textbox);
+		this.textBox = new TextBox(x, y, w, h);
+		ScrollBar scrollBar = new ScrollBar(Direction.HORIZONTAL, x, textBox.getHeight(), w, 10, textBox.getWidth(), textBox.getWidthText());
+		textBox.addAdjustListener(scrollBar);
+		scrollBar.addScrollBarListener(textBox);
+		this.addElement(textBox);
 		this.addElement(scrollBar);
+		
 	}
 	
-	public ScrollableTextBox(int x, int y,TextBox textbox) {
-		super(x,y,textbox.getWidth(),textbox.getHeight()+10);
-		ScrollBar scrollBar = new ScrollBar(Direction.HORIZONTAL, textbox.getX(), textbox.getHeight(), this.getWidth(), 10, textbox.getWidth(), textbox.getWidthText());
-		textbox.addAdjustListener(scrollBar);
-		scrollBar.addScrollBarListener(textbox);
-		this.addElement(textbox);
+	public ScrollableTextBox(int x, int y,TextBox textBox) {
+		super(x,y,textBox.getWidth(),textBox.getHeight()+10);
+		this.textBox = textBox;
+		ScrollBar scrollBar = new ScrollBar(Direction.HORIZONTAL, textBox.getX(), textBox.getHeight(), this.getWidth(), 10, textBox.getWidth(), textBox.getWidthText());
+		textBox.addAdjustListener(scrollBar);
+		scrollBar.addScrollBarListener(textBox);
+		this.addElement(textBox);
 		this.addElement(scrollBar);
 	}
 
 	public TextBox getTextBox() {
-		return textBox;
+		return this.textBox;
 	}
 
 }
